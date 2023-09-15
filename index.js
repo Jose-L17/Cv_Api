9fcac2308e7242c7d557c513af73044e6b39999e
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-09-14 20:11:14 -0600

    cambios

--- index.js
+++ index.js
@@ -21,6 +21,24 @@ app.get('/workexperience', (req, res, next) => {
 	res.sendfile("assets/work.json");
   });
 
+  
+app.get('/skills', (req, res, next) => {
+	res.sendfile("assets/skills.json");
+});
+
+app.get('/certificados', (req, res, next) => {
+	res.sendfile("assets/certiicados.json");
+});
+
+app.get('/education', (req, res, next) => {
+	res.sendfile("assets/education.json");
+});
+
+app.get('/interest', (req, res, next) => {
+	res.sendfile("assets/interest.json");
+});
+
+
 app.listen(port,  () => 
 	console.log('listening on port ' + port
 ));
