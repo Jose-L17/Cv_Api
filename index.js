28c230a8ef13d20cf8761b70fe1ff73fb8d07af1
Jose-L17 <100440883+Jose-L17@users.noreply.github.com>
2023-10-19 19:45:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -49,6 +49,23 @@ app.get('/size', (req, res, next) => {
 });
 
 
+app.get('/test1', (req, res, next) => {
+	res.sendfile("assets/3a_test1.json");
+});
+
+app.get('/test2', (req, res, next) => {
+	res.sendfile("assets/3a_test2.json");
+});
+
+app.get('/test3', (req, res, next) => {
+	res.sendfile("assets/3a_test3.json");
+});
+
+app.get('/test4', (req, res, next) => {
+	res.sendfile("assets/3a_test4.json");
+});
+
+
 app.listen(port,  () => 
 	console.log('listening on port ' + port
 ));
